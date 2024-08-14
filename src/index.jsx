a199714527263c15e781596cafde755e4017ebc5
hccdanny <117913283+HCCDanny@users.noreply.github.com>
2024-08-14 11:56:41 +0000

    Added skip to link

--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,17 @@ import r2wc from "@r2wc/react-to-web-component";
 import LoadExitThisSite from "./App";
 
 const exitThisSite = r2wc(LoadExitThisSite, { props: { url: "string" } });
+const skipLinkList = document.querySelector(".list--skip-links"),
+  addToSkipList = document.createElement("li"),
+  newLink = document.createElement("a");
+addToSkipList.className = "list__item";
+
+Object.assign(newLink, {
+  href: "https://bbc.co.uk/weather",
+  textContent: "Exit this site",
+  className: "list__link",
+});
+
+addToSkipList.appendChild(newLink);
+skipLinkList.appendChild(addToSkipList);
 customElements.define("exit-this-site", exitThisSite);
